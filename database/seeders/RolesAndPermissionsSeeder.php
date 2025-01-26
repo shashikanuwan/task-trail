418d1a2f94d92ccab551cdb7f9785fe7f3abfbcd
shashikanuwan <kumararanaweera1999@gmail.com>
2025-01-27 02:27:06 +0530

    move permissions to enum class

--- database/seeders/RolesAndPermissionsSeeder.php
+++ database/seeders/RolesAndPermissionsSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enums\Permission as EnumsPermission;
 use App\Enums\Role as EnumsRole;
 use Illuminate\Database\Seeder;
 use Spatie\Permission\Models\Permission;
@@ -15,35 +16,25 @@ class RolesAndPermissionsSeeder extends Seeder
         app()[PermissionRegistrar::class]->forgetCachedPermissions();
 
         $adminPermissions = [
-            'project create',      // Create projects
-            'project update',      // Update project details
-            'project delete',      // Delete projects
-            'project view all',    // View all projects and their details
-
-            'task create',         // Create tasks within a project
-            'task assign',         // Assign tasks to users
-            'task update',         // Update task details
-            'task change status',  // Change task status for any task
-            'task delete',         // Delete tasks
-
-            'task priority define', // Define task priority levels
-            'task priority update', // Update task priorities
-
-            'dashboard view all tasks', // View all tasks in the task status dashboard
-
-            'task completed notification receive', // Receive notifications when tasks are completed
+            EnumsPermission::PROJECT_CREATE,
+            EnumsPermission::PROJECT_UPDATE,
+            EnumsPermission::PROJECT_DELETE,
+            EnumsPermission::PROJECT_VIEW_ALL,
+            EnumsPermission::TASK_CREATE,
+            EnumsPermission::TASK_ASSIGN,
+            EnumsPermission::TASK_UPDATE,
+            EnumsPermission::TASK_DELETE,
+            EnumsPermission::TASK_PRIORITY_DEFINE,
+            EnumsPermission::TASK_PRIORITY_UPDATE,
+            EnumsPermission::TASK_COMPLETED_NOTIFICATION_RECEIVE,
         ];
 
         $userPermissions = [
-            'task view',           // View tasks assigned to them
-            'task update status',  // Update task status (Pending, In Progress, Completed)
-            'task comment',        // Add comments/notes on tasks (if applicable)
-
-            'dashboard view assigned tasks', // View tasks assigned to them in the task status dashboard
-
-            'notification receive', // Receive notifications when tasks are assigned or updated
-
-            'task assign notification receive', // Receive notifications when tasks are assigned to them
+            EnumsPermission::TASK_VIEW,
+            EnumsPermission::TASK_CHANGE_STATUS,
+            EnumsPermission::TASK_COMMENT,
+            EnumsPermission::DASHBOARD_VIEW_ASSIGNED_TASKS,
+            EnumsPermission::TASK_ASSIGN_NOTIFICATION_RECEIVE,
         ];
 
         $permissions = array_merge($adminPermissions, $userPermissions);
